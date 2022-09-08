8c969f0c6ecc9562fe6632a07556ffeac7729083
tarun <113051184+notifytarun@users.noreply.github.com>
2022-09-09 00:26:24 +0530

    Update settings.py

--- basic_learning/basic_learning/settings.py
+++ basic_learning/basic_learning/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7*l=(&+b2ynl=7&7&^-%o+7*p*2a&=rsgpgmkdibpin0dps=n)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tarun1haha.pythonanywhere.com']
 
 
 # Application definition
